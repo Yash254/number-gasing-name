74aa47b5d0c1485d41ddde1dbd0403edb94922c5
Yash254 <72601812+Yash254@users.noreply.github.com>
2021-10-03 14:56:37 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,12 @@
+age = int(input("enter your age-"))
+if(age > 18):
+    print("your an adult")
+elif(age >= 12):
+    print("your a teenager")
+else:
+    print("your a kid")
+
+count=5
+while(count>0):
+    print(count)
+    count=count-1
